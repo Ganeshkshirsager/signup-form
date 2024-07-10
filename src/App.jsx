354d4b9618ff2299f3a2ca4c6127bf2011750b9f
Ganesh Kshirsagar <kshrisagerganesh@gmail.com>
2024-07-10 10:52:45 +0530

    Updated signup form and added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignUpForm from './components/SignUpForm';
+import LoginPage from './components/LoginPage';
 
 const App = () => {
   return (
-    <div className="App">
-      <SignUpForm />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signup" component={SignUpForm} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={SignUpForm} /> {/* Default route */}
+      </Switch>
+    </Router>
   );
 };
 
